37e238dec37660b7a849744bfff79c622579b910
HugoSev1 <hugosevrain@gmail.com>
2022-04-08 21:45:38 +0200

    insertion d'un nouveau produit en bdd et redirection

--- src/Controller/ManageProductController.php
+++ src/Controller/ManageProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\Type\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ManageProductController extends AbstractController
 {
     #[Route('manage/product/new', name: 'manage_product_new')]
-    public function new(Request $request): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
         $product = new Product();
 
@@ -24,7 +25,14 @@ class ManageProductController extends AbstractController
         $form->add('Ajouter', SubmitType::class); // permet d'ajouter un champ à ceux prévus dans la classe ProductType
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            // mise à jour de la date de création
+            $product->setCreatedAt(new \DateTimeImmutable());
             // persister l'objet en bdd
+            $em->persist($product);
+            // synchronisation des objets persistés dans la bdd
+            $em->flush();
+
+            return $this->redirectToRoute('product_show_all');
         }
         return $this->renderForm('product/product_new.html.twig',
             [
