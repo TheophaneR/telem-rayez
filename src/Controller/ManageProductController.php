6e4dc4960a82ebf521c4ac6eb898ade01a8b5ed7
HugoSev1 <hugosevrain@gmail.com>
2022-04-08 21:01:29 +0200

    récupération des données du formulaire ok

--- src/Controller/ManageProductController.php
+++ src/Controller/ManageProductController.php
@@ -6,13 +6,14 @@ use App\Entity\Product;
 use App\Form\Type\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ManageProductController extends AbstractController
 {
     #[Route('manage/product/new', name: 'manage_product_new')]
-    public function new(): Response
+    public function new(Request $request): Response
     {
         $product = new Product();
 
@@ -21,6 +22,10 @@ class ManageProductController extends AbstractController
             $product,
         );
         $form->add('Ajouter', SubmitType::class); // permet d'ajouter un champ à ceux prévus dans la classe ProductType
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            echo 'Le formulaire a été validé.';
+        }
         return $this->renderForm('product/product_new.html.twig',
             [
                 'form' => $form
